a11018abc277ec7a00fbb6d1c1e2490aaa36552b
kdw <spnova12@gmail.com>
2018-11-05 16:30:09 +0900

    .

--- Set5_psnr_tset.py
+++ Set5_psnr_tset.py
@@ -3,6 +3,7 @@ import cv2
 from os.path import join
 from os import listdir
 from math import log10
+import matlab_imresize
 
 
 class Resize(object):
@@ -11,11 +12,11 @@ class Resize(object):
 
     def __call__(self, img):
         dsize = (int(np.shape(img)[1] / self.scale_factor), int(np.shape(img)[0] / self.scale_factor))
-        return cv2.resize(img, dsize=dsize, interpolation=cv2.INTER_CUBIC)
+        return matlab_imresize.imresize(img, output_shape=dsize)
+        #return cv2.resize(img, dsize=dsize, interpolation=cv2.INTER_CUBIC)
 
 
 def get_psnr(img1, img2, min_value=0, max_value=255):
-
     mse = np.mean((img1 - img2) ** 2)
     if mse == 0:
         return 100
@@ -26,30 +27,31 @@ def get_psnr(img1, img2, min_value=0, max_value=255):
 origin_test_dir = 'Set5'
 origin_tests = [join(origin_test_dir, x) for x in sorted(listdir(origin_test_dir))]
 
+resize = Resize(4)
+resize_inverse = Resize(1 / 4)
+
 psnrs = []
 
 for origin_test in origin_tests:
     img_hr = cv2.imread(origin_test, cv2.IMREAD_COLOR)
 
-    resize = Resize(4)
-    resize_inverse = Resize(1 / 4)
-    
+    img_hr = cv2.cvtColor(img_hr, cv2.COLOR_BGR2YCrCb)
+
+    img_hr = img_hr.astype(np.float64)
+
     img_lr = resize(img_hr)
     img_hr_new = resize_inverse(img_lr)
 
-    img_hr = cv2.cvtColor(img_hr, cv2.COLOR_BGR2YUV)[4:-4, 4:-4, 0]
-    img_hr_new = cv2.cvtColor(img_hr_new, cv2.COLOR_BGR2YUV)[4:-4, 4:-4, 0]
+    img_hr = img_hr[4:-4, 4:-4, 0]
+    img_hr_new = img_hr_new[4:-4, 4:-4, 0]
 
-    psnr = get_psnr(img_hr.astype(np.float32), img_hr_new.astype(np.float32), 0, 255)
+    img_hr = img_hr.clip(0, 255)
+    img_hr_new = img_hr_new.clip(0, 255)
+
+    img_hr = img_hr.astype(np.uint8)
+    img_hr_new = img_hr_new.astype(np.uint8)
+
+    psnr = get_psnr(img_hr.astype(np.float64), img_hr_new.astype(np.float64), 0, 255)
     psnrs.append(psnr)
-
-    print(psnr)
-
-    # cv2.imshow('image', img_hr)
-    # cv2.waitKey(1000)
-    # cv2.destroyAllWindows()
-    # cv2.imshow('image', img_hr_new)
-    # cv2.waitKey(1000)
-    # cv2.destroyAllWindows()
 
 print('mean psnr :', np.mean(psnrs))
